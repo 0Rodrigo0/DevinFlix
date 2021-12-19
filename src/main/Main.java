c206c85460b478496878d18c7333fe8561f8592e
0Rodrigo0 <rodrigobl1@yahoo.com.br>
2021-12-19 18:12:01 -0300

    Lista de usuarios

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,66 +1,78 @@
 package main;
 
-import java.util.Date;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 import usuarios.Usuario;
 
-
 public class Main {
-    
+
     public static void main(String[] args) {
-
-        System.out.println("_________________________________________");
-        System.out.println(" ");
-        System.out.println("****** BEM VINDO AO DevinFlix ******** ");
-        System.out.println("_________________________________________");
-
-        System.out.println(" ");
-        System.out.println("Vamos começar criando seu cadastro.");
-        System.out.println(" ");
-
-        String nome;
-        String endereco;
-        String dataNascimento;
-
-        //inicia scanner
-        Scanner entrada = new Scanner(System.in);
-        System.out.println("Pressione ENTER para começar...");
-
-        //aguarda o enter
-        entrada.nextLine();
-        
-        do {
-            System.out.println(" ");
-            System.out.println("Por favor digite seu nome APENAS com Letras!");
-            System.out.println(" ");
-		    System.out.print("Digite seu Nome completo: ");
-            nome = entrada.nextLine();
-        } while (nome.matches("[0-9]*") | nome.matches("[ ]*"));
-        
-        do {
-            System.out.println("_________________________________________");
-            System.out.println("Digite seu endereço com numero!");
-            System.out.println(" ");
-            System.out.print("Digite seu Endereço: ");
-            endereco = entrada.nextLine();
-        } while (endereco.matches("[ ]*") | endereco.matches("[a-z]*") | endereco.matches("[0-9]*") | endereco.matches("[A-Z]*"));
-        
-        do {
-            System.out.println("_________________________________________");
-            System.out.print("Digite sua data de nascimento: ");
-            dataNascimento = entrada.nextLine();
-        } while (dataNascimento.matches("[ ]*") | dataNascimento.matches("[a-z]*") | dataNascimento.matches("[A-Z]*") | dataNascimento.matches("[0-9]*"));
-        
-        entrada.close();
-        System.out.println(" ");
-        Usuario u1 = new Usuario(nome, endereco, dataNascimento);
-        System.out.println("Seja muito bem vindo: " + u1.nomeUsuario + ".");
-        System.out.println("O endereço cadastrado é: " + u1.enderecoUsuario + ".");
-        System.out.println("Sua data de nascimento: " + u1.dataNacimentoUsuario + ".");
+        menuInicial();
     }
 
-    
+    public static void menuInicial() {
+        Usuario u1;
+        Scanner inicio = new Scanner(System.in);
+        int opcao = 0;
+        List<Usuario> relacaoUsuario = new ArrayList<Usuario>();
+        do {
+
+            System.out.println("****** BEM VINDO AO DevinFlix ******** ");
+            System.out.println("O que você deseja fazer?");
+            System.out.println(" ");
+            System.out.println("Digite 1 para cadastrar um usuário.");
+            System.out.println("Digite 2 para ver lista de usuários.");
+            System.out.println("Digite 3 para ver lista de filmes.");
+            System.out.println("Digite 4 para curtir/descurtir um filme.");
+            System.out.println("Digite 5 para cadastrar um filme.");
+            System.out.println("Digite 6 para indicar um filme.");
+            System.out.println("Digite 7 para sair.");
+            System.out.println(" ");
+            opcao = Integer.parseInt(inicio.nextLine());
+            System.out.println("_________________________________________");
+
+            if (opcao == 1) {
+                System.out.println(" ");
+                System.out.println("Vamos começar a criar seu cadastro.");
+                u1 = new Usuario();
+                do {
+                    System.out.println("Por favor digite seu nome APENAS com Letras!");
+                    System.out.println(" ");
+                    System.out.print("Digite seu Nome completo: ");
+                    u1.setNomeUsuario(inicio.nextLine());
+                } while (u1.nomeUsuario.matches("[0-9]*") | u1.nomeUsuario.matches("[ ]*"));
+
+                do {
+                    System.out.println("_________________________________________");
+                    System.out.println("Digite seu endereço com numero!");
+                    System.out.println(" ");
+                    System.out.print("Digite seu Endereço: ");
+                    u1.setEnderecoUsuario(inicio.nextLine());
+                } while (u1.enderecoUsuario.matches("[ ]*") | u1.enderecoUsuario.matches("[a-z]*")
+                        | u1.enderecoUsuario.matches("[0-9]*")
+                        | u1.enderecoUsuario.matches("[A-Z]*"));
+
+                do {
+                    System.out.println("_________________________________________");
+                    System.out.print("Digite sua data de nascimento: ");
+                    u1.setDataNacimentoUsuario(inicio.nextLine());
+                } while (u1.dataNacimentoUsuario.matches("[ ]*") | u1.dataNacimentoUsuario.matches("[a-z]*")
+                        | u1.dataNacimentoUsuario.matches("[A-Z]*")
+                        | u1.dataNacimentoUsuario.matches("[0-9]*"));
+
+                System.out.println(" ");
+                relacaoUsuario.add(u1);
+            }
+            if (opcao == 2) {
+                System.out.println("_________________________________________");
+                System.out.println(relacaoUsuario.toString());
+                System.out.println("_________________________________________");
+            }
+        } while (opcao != 7);
+        System.out.println("Obrigado!");
+        inicio.close();
+    }
+
 }
-
-
--- src/usuarios/Usuario.java
+++ src/usuarios/Usuario.java
@@ -6,13 +6,45 @@ public class Usuario {
     public String nomeUsuario;
     public String enderecoUsuario;
     public String dataNacimentoUsuario;
-    
-    // forma de criar usuario
-    public Usuario(String nomeUsuario, String enderecoUsuario, String dataNacimentoUsuario) {
 
+    // forma de criar usuario
+    // public Usuario(String nomeUsuario, String enderecoUsuario, String
+    // dataNacimentoUsuario) {
+    //
+    // this.nomeUsuario = nomeUsuario;
+    // this.enderecoUsuario = enderecoUsuario;
+    // this.dataNacimentoUsuario = dataNacimentoUsuario;
+    // }
+
+    public String getNomeUsuario() {
+        return nomeUsuario;
+    }
+
+    // Getther e Setters
+    public void setNomeUsuario(String nomeUsuario) {
         this.nomeUsuario = nomeUsuario;
+    }
+
+    public String getEnderecoUsuario() {
+        return enderecoUsuario;
+    }
+
+    public void setEnderecoUsuario(String enderecoUsuario) {
         this.enderecoUsuario = enderecoUsuario;
+    }
+
+    public String getDataNacimentoUsuario() {
+        return dataNacimentoUsuario;
+    }
+
+    public void setDataNacimentoUsuario(String dataNacimentoUsuario) {
         this.dataNacimentoUsuario = dataNacimentoUsuario;
     }
-    
+
+    @Override
+    public String toString() {
+        return "Nome: " + nomeUsuario + ". Endereço: " + enderecoUsuario + ". Data de Nascimento: "
+                + dataNacimentoUsuario + "\n";
+    }
+
 }
