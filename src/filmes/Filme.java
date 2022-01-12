0350f65c5b0449fcb9f2811b6df70e105fab2e15
0Rodrigo0 <rodrigobl1@yahoo.com.br>
2022-01-12 14:03:46 -0300

    reiniciando

--- src/filmes/Filme.java
+++ src/filmes/Filme.java
@@ -1,12 +1,23 @@
 package filmes;
 
-// Sempre com letra maiuscula
+import java.net.URL;
+import java.time.LocalDate;
+
 public class Filme {
 
-    public String nome;
-    public String genero;
-    public String sinopse;
-    public String link;
+    private String nome;
+    private String sinopse;
+    private Genero genero;
+    private URL link;
+    private LocalDate anoLancamento;
+
+    public LocalDate getAnoLancamento() {
+        return anoLancamento;
+    }
+
+    public void setAnoLancamento(LocalDate anoLancamento) {
+        this.anoLancamento = anoLancamento;
+    }
 
     public String getNome() {
         return nome;
@@ -14,14 +25,6 @@ public class Filme {
 
     public void setNome(String nome) {
         this.nome = nome;
-    }
-
-    public String getGenero() {
-        return genero;
-    }
-
-    public void setGenero(String genero) {
-        this.genero = genero;
     }
 
     public String getSinopse() {
@@ -32,20 +35,20 @@ public class Filme {
         this.sinopse = sinopse;
     }
 
-    public String getLink() {
+    public Genero getGenero() {
+        return genero;
+    }
+
+    public void setGenero(Genero genero) {
+        this.genero = genero;
+    }
+
+    public URL getLink() {
         return link;
     }
 
-    public void setLink(String link) {
+    public void setLink(URL link) {
         this.link = link;
-    }
-
-    @Override
-    public String toString() {
-        // o metodo precisa do retorno para ser impresso depois na Main
-        return "Nome: " + nome + "\n";
-        // return "Nome: " + nome + ". Genero: " + genero + ". Sinopse: "
-        // + sinopse + ". Link: " + link + "\n";
     }
 
 }
---
+++ src/usuarios/Usuarios.java
@@ -0,0 +1,35 @@
+package usuarios;
+
+import java.time.LocalDate;
+
+public class Usuarios {
+
+    private String nome;
+    private String endereco;
+    private LocalDate dataNascimento;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public LocalDate getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(LocalDate dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+}
